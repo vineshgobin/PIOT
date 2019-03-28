b430a3a91fae1f9df04eefff2d0f4472c575a74d
Unknown <vineshgobin@gmail.com>
2019-03-29 05:53:02 +1100

    Pushbullet push notif
    Pushes a notification to phone

---
+++ pushbullet_mesage.py
@@ -0,0 +1,9 @@
+import requests
+
+class pushbullet:
+    def push_note(self, title, message):
+        url = 'https://api.pushbullet.com/v2/pushes'
+        headers = { 'Access-Token': 'o.3BURPKcWSiMAJlQpFzw0EwxS7A5fWk7P' }
+        data = {'title': title, 'body': message, 'type': 'note'}
+        r = requests.post(url, data=data, headers=headers).json()
+        print('Pushed: ' + message)

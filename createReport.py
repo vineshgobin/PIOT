c5438f40cf47de5552c3a8815017e15b3c852b6a
Unknown <mananlodhia@hotmail.com>
2019-03-30 20:28:30 +1100

    Update on CSV

--- createReport.py
+++ createReport.py
@@ -9,12 +9,14 @@ class createReport:
         # Connects to database
         conn = sqlite3.connect('/home/pi/IOT-A1/sensehat.db')
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM SENSEHAT_data WHERE Humidity > 0;")
+        cursor.execute("SELECT DateTime,Temp,Humidity FROM SENSEHAT_data WHERE (Temp < 20 OR Temp > 30) AND (Humidity < 50 OR Humidity > 60)")
+        
 
         # Writes report
-        with open("/home/pi/IOT-A1/report.csv", "w") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([i[0] for i in cursor.description]) # write headers
+        with open("report.csv", "w") as csv_file:
+            csv_writer = csv.writer(csv_file, quoting = csv.QUOTE_MINIMAL)
+            #csv_writer.writerow([i[0] for i in cursor.description]) # write headers
+            csv_writer.writerow(["Date: Status"])
             csv_writer.writerows(cursor)
 
 createReport.report()

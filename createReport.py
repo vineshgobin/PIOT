3d00127d1fa5d3f9ebe239834787e57ba605d407
Unknown <mananlodhia@hotmail.com>
2019-04-01 16:15:01 +1100

    Updated Report Gathering

--- createReport.py
+++ createReport.py
@@ -1,22 +1,65 @@
-import csv
+import json
 import sqlite3
-from os import path
+from datetime import datetime, timedelta
+ 
 
-class createReport:
 
-    def report():
+DB_NAME = "sensehat.db"
+DATE_FORMAT = "%Y-%m-%d"
+ONE_DAY_DELTA = timedelta(days = 1)
 
-        # Connects to database
-        conn = sqlite3.connect('/home/pi/IOT-A1/sensehat.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT DateTime,Temp,Humidity FROM SENSEHAT_data WHERE (Temp < 20 OR Temp > 30) AND (Humidity < 50 OR Humidity > 60)")
+with open('config.json') as f:
+    data = json.load(f)
+
+# Main function.
+def main():
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row
+    with connection:
+        cursor = connection.cursor()
+
+        row = cursor.execute("SELECT DATE(MIN(DateTIME)), DATE(MAX(DateTime)) FROM sensehat_data").fetchone()
+        startDate = datetime.strptime(row[0], DATE_FORMAT)
+        endDate = datetime.strptime(row[1], DATE_FORMAT)
+
+        print("Dates:")
+        date = startDate
+        while date <= endDate:
+            row = cursor.execute(
+                """SELECT MIN(Temp), MAX(Temp), MIN(Humidity), MAX(Humidity) FROM sensehat_data
+                WHERE DateTime >= DATE(:date) AND DateTime < DATE(:date, '+1 day')""",
+                { "date": date.strftime(DATE_FORMAT) }).fetchone()
         
+        
+            minTemp = row[0]
+            maxTemp = row[0]
+            minHum = row[1]
+            maxHum = row[1]
 
-        # Writes report
-        with open("report.csv", "w") as csv_file:
-            csv_writer = csv.writer(csv_file, quoting = csv.QUOTE_MINIMAL)
-            #csv_writer.writerow([i[0] for i in cursor.description]) # write headers
-            csv_writer.writerow(["Date: Status"])
-            csv_writer.writerows(cursor)
+            configMinTemp = data["min_temperature"]
+            configMaxTemp = data["max_temperature"]
+            configMinHum = data["min_humidity"]
+            configMaxHum = data["max_humidity"]
 
-createReport.report()
+            message = "OK"
+            if(minTemp < configMinTemp):
+                message = "BAD: below minimum temp" + minTemp
+            if(maxTemp > configMinTemp):
+                message = "BAD: above maximum temp" + maxTemp
+            if(minHum < configMinHum):
+                message = "BAD: below minimum humidity" +minHum
+            if(maxHum > configMaxHum):
+                message = "BAD: above maximum humidity" + maxHum
+            else:
+                message 
+            
+            #a = int(requests.form['maxHum - configMaxHum'])
+            
+            print(date.strftime(DATE_FORMAT) + " | " + message)
+           
+            date += ONE_DAY_DELTA
+    connection.close()
+
+# Execute program.
+if __name__ == "__main__":
+    main()
